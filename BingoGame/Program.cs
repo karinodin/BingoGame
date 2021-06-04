a7eff521b5d32f3920789b4ffe770f0aaac8c8e8
Karin Odin <Karin.odin@byggtjanst.se>
2021-06-04 15:40:08 +0200

    Create console app

---
+++ BingoGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BingoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = 10;
+            var bingoBoard = new BingoBoard(rows);
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class BingoBoard
+    {
+        private int _rows;
+
+        public BingoBoard(int rows)
+        {
+            _rows = rows;
+        }
+    }
+}
